f37c6e8aa353fb0ff848d7ed44b8719d65b4fd41
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2025-04-14 18:13:23 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React from 'react';
+import Links from './Links';
 
-function About() {
-  return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
+function About({ bio, links }) {
+	return (
+		<div id="about">
+			<h2>About Me</h2>
+
+			{/* Conditional rendering */}
+			{/* {bio ? <p>{bio}</p> : null} */}
+
+			{bio && <p>{bio}</p>}
+
+			<img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+			{/* add your <Links /> component here */}
+			{/* <Links github={links.github} linkedin={links.linkedin} /> */}
+			{/* Props drilling
+      Passing down props more than 1 level deep
+       */}
+			<Links {...links} />
+		</div>
+	);
 }
 
 export default About;
